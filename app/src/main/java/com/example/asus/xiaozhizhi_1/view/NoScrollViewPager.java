1e4b5d431303e3c2ec393c8dd6c4029f2d3045ba
hexianzhi <hxz707950194@gmail.com>
2016-08-24 15:26:07 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/asus/xiaozhizhi_1/view/NoScrollViewPager.java
+++ app/src/main/java/com/example/asus/xiaozhizhi_1/view/NoScrollViewPager.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
- 
+
 public class NoScrollViewPager extends ViewPager {
 
 	public NoScrollViewPager(Context context, AttributeSet attrs) {
